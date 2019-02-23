417cb45e60d221a6a3ac9b50e4dbebb966628d6f
luckysle7in <luckysle7in@ya.ru>
2019-02-24 00:15:43 +0100

    Syntax fix

--- src/get-filter.js
+++ src/get-filter.js
@@ -3,5 +3,5 @@ export default (title, isChecked = false) => {
   return `
     <input type="radio" id="filter-${filterName}" name="filter" value="${filterName}" ${isChecked ? `checked` : ``}>
     <label class="trip-filter__item" for="filter-${filterName}">${title}</label>
-  `
-}
+  `;
+};
--- src/main.js
+++ src/main.js
@@ -6,14 +6,14 @@ const eventsNode = document.getElementById(`trip-day__items`);
 
 // Array with content for filters
 const filters = [
-  { name: `Everything`, isChecked: true },
-  { name: `Future`, isChecked: false },
-  { name: `Past`, isChecked: false }
+  {name: `Everything`, isChecked: true},
+  {name: `Future`, isChecked: false},
+  {name: `Past`, isChecked: false}
 ];
 
 // Get code for the list of the filters
 let filtersCode = ``;
-filters.forEach((filter, i) => {
+filters.forEach((filter) => {
   filtersCode += getFilter(filter.name, filter.isChecked);
 });
 filtersNode.innerHTML = filtersCode;
@@ -25,14 +25,14 @@ const getEventCode = (number) => {
     eventsCode += getEvent();
   }
   return eventsCode;
-}
+};
 eventsNode.innerHTML = getEventCode(7);
 
 // Show new events, if a filter has changes. Number of the events is taken from filter that has chosen
 const filtersLinks = filtersNode.getElementsByClassName(`trip-filter__item`);
 for (let filtersLink of filtersLinks) {
   // Click event for each filter
-  filtersLink.addEventListener(`click`, (event) => {
+  filtersLink.addEventListener(`click`, () => {
     eventsNode.innerHTML = getEventCode(Math.floor(Math.random() * 9 + 1));
   });
 }
